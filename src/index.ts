64760ab1f67347d247cdedac1ded202e105e55d1
lcbhk <lcbhk@icloud.com>
2019-12-18 17:56:49 +0800

    init commit

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,3 @@ const webGLbtn = document.createElement("button");
 webGLbtn.innerText = "webgl";
 document.body.appendChild(webGLbtn);
 webGLbtn.addEventListener("click", webgl);
-
-const drawLine = document.querySelector('#draw-line');
-drawLine.addEventListener('click', () => {
-  const canvas = document.querySelector('#c') as HTMLCanvasElement;
-  const gl = canvas.getContext('webgl');
-  
-  const vertexSource = `
-
-  `
-  const fragmentSource = `
-    
-  `
-})
