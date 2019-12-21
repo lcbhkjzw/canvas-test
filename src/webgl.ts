af384728c9e8029273a0fa72770dc8a6e0e46e10
lcbhk <lcbhk@cloud.com>
2019-12-21 11:06:49 +0800

    从外面传入顶点数据

--- src/webgl.ts
+++ src/webgl.ts
@@ -4,9 +4,11 @@ export default function webgl() {
   const canvas = document.querySelector("#c") as HTMLCanvasElement;
   const gl = canvas.getContext("webgl");
   const vertexShaderSource = `
+    attribute vec4 a_position;
+
     void main() {
-      gl_Position     = vec4(.0, .0, .0, 1.0);
-      gl_PointSize     = 100.0;
+      gl_Position = a_position;
+      gl_PointSize = 100.0;
     }
   `;
   const fragmentShaderSource = `
@@ -22,6 +24,18 @@ export default function webgl() {
   );
   const program = createProgram(gl, vertexShader, fragmentShader);
 
+  //传递顶点数据给shader
+  const positionAttribute = gl.getAttribLocation(program, "a_position");
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  const position = [-0.2, 0.2];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(position), gl.STATIC_DRAW);
+
   gl.useProgram(program);
+  gl.enableVertexAttribArray(positionAttribute);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.vertexAttribPointer(positionAttribute, 2, gl.FLOAT, false, 0, 0);
+
   gl.drawArrays(gl.POINTS, 0, 1);
 }
