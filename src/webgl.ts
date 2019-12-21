e1324b3caf03f96d4f4ce3ee5c808db8d85ea30e
lcbhk <lcbhk@cloud.com>
2019-12-21 10:37:26 +0800

    画点

--- src/webgl.ts
+++ src/webgl.ts
@@ -5,24 +5,14 @@ function webgl() {
   const canvas = document.querySelector("#c") as HTMLCanvasElement;
   const gl = canvas.getContext("webgl");
   const vertexShaderSource = `
-    attribute vec2 a_position;
-
-    uniform vec2 u_resolution;
-
-    void main () {
-      vec2 zeroToOne = a_position / u_resolution;
-      vec2 zeroToTwo = zeroToOne * 2.0;
-      vec2 clipSpace = zeroToTwo - 1.0;
-      gl_Position = vec4(clipSpace * vec2(1,-1), 0, 1);
+    void main() {
+      gl_Position     = vec4(.0, .0, .0, 1.0);
+      gl_PointSize     = 100.0;
     }
   `;
   const fragmentShaderSource = `
-    precision mediump float;
-
-    uniform vec4 u_color;
-
-    void main () {
-      gl_FragColor = u_color;
+    void main() {
+      gl_FragColor = vec4(.0, .0, 1.0, 1.0);
     }
   `;
   const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
@@ -33,81 +23,8 @@ function webgl() {
   );
   const program = createProgram(gl, vertexShader, fragmentShader);
 
-  const positionLocation = gl.getAttribLocation(program, "a_position");
-  const resolutionLocation = gl.getUniformLocation(program, "u_resolution");
-  const colorLocation = gl.getUniformLocation(program, "u_color");
-  const positionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  const positions = [10, 20, 80, 20, 10, 30, 10, 30, 80, 20, 80, 30];
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  gl.clearColor(0, 0, 0, 0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
   gl.useProgram(program);
-  gl.enableVertexAttribArray(positionLocation);
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
-
-  const size = 2;
-  const type = gl.FLOAT;
-  const normalize = false;
-  const stride = 0;
-  const offset = 0;
-  gl.vertexAttribPointer(
-    positionLocation,
-    size,
-    type,
-    normalize,
-    stride,
-    offset
-  );
-
-  for (let ii = 0; ii < 50; ++ii) {
-    generateRect(
-      gl,
-      randomInt(300),
-      randomInt(300),
-      randomInt(300),
-      randomInt(300)
-    );
-
-    gl.uniform4f(colorLocation, Math.random(), Math.random(), Math.random(), 1);
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
-  }
-  setTimeout(() => {
-    console.time('readPixels')
-    let pixels = new Uint8Array(
-      gl.drawingBufferWidth * gl.drawingBufferHeight * 4
-    );
-    gl.readPixels(
-      0,
-      0,
-      gl.drawingBufferWidth,
-      gl.drawingBufferHeight,
-      gl.RGBA,
-      gl.UNSIGNED_BYTE,
-      pixels
-    );
-    console.timeEnd('readPixels')
-  }, 1000);
-}
-
-function generateRect(gl, x, y, width, height) {
-  const x1 = x;
-  const x2 = x + width;
-  const y1 = y;
-  const y2 = y + height;
-
-  gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]),
-    gl.STATIC_DRAW
-  );
-}
-
-function randomInt(range) {
-  return Math.floor(Math.random() * range);
+  gl.drawArrays(gl.POINTS, 0, 1)
 }
 
 /*******************  3d ******************/
